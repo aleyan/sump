6fc52145c7e6850b18e9d007aea9ec22d7bae033
Alex Yankov <yankov.alex@gmail.com>
2024-07-13 23:40:08 -0400

    print to screen by default

--- sump/main.py
+++ sump/main.py
@@ -4,14 +4,18 @@ from .parser import summarize_project
 
 @click.command()
 @click.option('--directory', '-d', default='.', help='Directory to summarize')
+@click.option('--output_file', '-o', default=None, help='Write summary to file')
 @click.option('--include', '-i', multiple=True, help='Files to include fully')
-def main(directory: str, include: tuple):
+def main(directory: str, output_file: str, include: tuple):
     summary = summarize_project(directory, include)
     
-    with open('dump.txt', 'w') as dump_file:
-        dump_file.write(summary)
+    if output_file is not None:
+        with open(output_file, 'w') as dump_file:
+            dump_file.write(summary)
+        click.echo(f"Project summary written to dump.txt")
+    else:
+        print(summary)
     
-    click.echo(f"Project summary written to dump.txt")
 
 if __name__ == '__main__':
     main()
